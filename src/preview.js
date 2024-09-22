d77af035e2bf78ac5a043a4bf3d11e27695a210a
kiprudroid <kiprutoshadrack06@gmail.com>
2024-09-22 23:28:15 +0300

    Added the compose page

---
+++ src/preview.js
@@ -0,0 +1,26 @@
+import react from React ;
+import parse from 'html-react-parser';
+
+function Preview(props) {
+
+
+    return (
+        <>
+        <div className="col-lg-10 col-md-10 col-sm-12">
+
+            <div className="row">
+                <div className="col-lg-8 col-md-8 col-sm-10" style={{overflowY : "scroll", height : "100vh", borderRight : "1px solid rgb(41,41,41)"}}>
+
+                    <div className="row border border-white p-3 m-auto col-lg-8 col-md-8 col-sm-12 m-auto mt-3  border rounded" style={{
+                        background : "white"
+                    }}>
+                        <div className="col-lg-8 col-md-8 col-sm-10">
+                        {parse(props.content)}
+                        </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        </>
+    )
+}
